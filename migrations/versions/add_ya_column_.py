206089d4a911d90a248eeedda76b38f044baf5eb
Henninglar <124566110+Henninglar@users.noreply.github.com>
2024-04-02 11:23:32 +0200

    Add files via upload

---
+++ migrations/versions/add_ya_column_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: add_ya_column
+Revises: 39550e540d9e
+Create Date: 2023-10-17 19:22:29.198364
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'add_ya_column'
+down_revision = '39550e540d9e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
